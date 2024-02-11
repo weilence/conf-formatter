a95cedaf59fd68d01ca1daa0d3142fe7ff9c5757
Weilence <weilence@163.com>
2024-02-11 21:35:04 +0800

    refactor: symbol use listener

--- src/symbol.ts
+++ src/symbol.ts
@@ -1,8 +1,8 @@
 import * as vscode from 'vscode';
 import { parse } from './parser';
 import NamedConfListener from './grammer/NamedConfListener';
-import { ParseTreeWalker } from 'antlr4';
-import { StatementContext } from './grammer/NamedConfParser';
+import { ParseTreeWalker, ParserRuleContext, TerminalNode } from 'antlr4';
+import NamedConfParser, { ProgContext, StatementContext, StructContext } from './grammer/NamedConfParser';
 
 const selector = {language: 'named.conf'};
 
@@ -13,69 +13,11 @@ function provideDocumentSymbols(document: vscode.TextDocument):vscode.DocumentSy
   const text = document.getText(textRange);
   const ctx = parse(text);
 
-  const documentSymbols: vscode.DocumentSymbol[] = [];
-  const listener = new NamedConfListener();
-
-  const symbolStack: vscode.DocumentSymbol[] = [];
-  listener.enterStatement = (ctx: StatementContext) => {
-    const [name, kind] = getStatementContext(ctx);
-    if (kind === null) {
-      return;
-    }
-
-    const docSymbol = new vscode.DocumentSymbol(
-      name,
-      '',
-      kind,
-      new vscode.Range(
-        document.positionAt(ctx.start.start),
-        document.positionAt(ctx.stop?.stop ?? ctx.start.stop)
-      ),
-      new vscode.Range(
-        document.positionAt(ctx.start.start),
-        document.positionAt(ctx.stop?.stop ?? ctx.start.stop)
-      )
-    );
-
-    if (symbolStack.length === 0) {
-      documentSymbols.push(docSymbol);
-    } else {
-      symbolStack[symbolStack.length - 1].children.push(docSymbol);
-    }
-
-    symbolStack.push(docSymbol);
-  };
-  listener.exitStatement = () => {
-    symbolStack.pop();
-  };
-
+  const listener = new SymbolListener();
   const walker = new ParseTreeWalker();
   walker.walk(listener, ctx);
 
-  return documentSymbols;
-}
-
-function getStatementContext(ctx: StatementContext): [string, vscode.SymbolKind | null] {
-  const valueList = ctx.value_list();
-  const first = valueList[0];
-  if (!first) {
-    return ['', null];
-  } else if (first.struct()) {
-    return ['{}', vscode.SymbolKind.Struct];
-  }
-
-  const name = first.getText();
-  if (valueList[1]?.struct() || valueList[2]?.struct()) {
-    return [name, vscode.SymbolKind.Struct];
-  }
-
-  if (first.IDENTIFIER()) {
-    return [name, vscode.SymbolKind.Field];
-  } else if (first.STRING()) {
-    return [name, vscode.SymbolKind.String];
-  }
-
-  return [name, null];
+  return listener.documentSymbols;
 }
 
 export default function registerSymbolProvider(context: vscode.ExtensionContext) {
@@ -83,4 +25,81 @@ export default function registerSymbolProvider(context: vscode.ExtensionContext)
     selector,
     { provideDocumentSymbols },
   ));
+}
+
+class SymbolListener extends NamedConfListener {
+  documentSymbols: vscode.DocumentSymbol[] = [];
+  private map: Map<ParserRuleContext, vscode.DocumentSymbol> = new Map();
+
+  exitStruct = (ctx: ParserRuleContext) => {
+    this.map.delete(ctx);
+  };
+
+  enterStatement = (ctx: StatementContext) => {
+    const docSymbol = this.getStatementContext(ctx);
+    if (!docSymbol) {
+      return;
+    }
+
+    for (const item of ctx.value_list()) {
+      const struct = item.struct();
+      if (struct) {
+        this.map.set(struct, docSymbol);
+      }
+    }
+
+    const parent = ctx.parentCtx;
+    if (parent instanceof ProgContext) {
+      this.documentSymbols.push(docSymbol);
+    }
+
+    if (parent instanceof StructContext) {
+      const parentDocumentSymbol = this.map.get(parent);
+      if (parentDocumentSymbol) {
+        if (!parentDocumentSymbol.children) {
+          parentDocumentSymbol.children = [];
+        }
+        parentDocumentSymbol.children.push(docSymbol);
+      }
+    }
+  };
+
+
+  getStatementContext(ctx: StatementContext): vscode.DocumentSymbol | null {
+    const value = ctx.value(0)?.getChild(0);
+
+    let name: string;
+    let kind: vscode.SymbolKind;
+    if (value instanceof StructContext) {
+      name = '{}';
+      kind = vscode.SymbolKind.Struct;
+    } else if (value instanceof TerminalNode) {
+      name = value.getText();
+      if (value.symbol.type === NamedConfParser.STRING) {
+        kind = vscode.SymbolKind.String;
+      } else {
+        kind = vscode.SymbolKind.Field;
+      }
+    } else {
+      return null;
+    }
+
+    return new vscode.DocumentSymbol(
+      name,
+      '',
+      kind,
+      new vscode.Range(
+        ctx.start.line - 1,
+        ctx.start.column,
+        (ctx.stop?.line ?? ctx.start.line) - 1,
+        ctx.stop?.column ?? ctx.start.column,
+      ),
+      new vscode.Range(
+        ctx.start.line - 1,
+        ctx.start.column,
+        (ctx.stop?.line ?? ctx.start.line) - 1,
+        ctx.stop?.column ?? ctx.start.column,
+      )
+    );
+  }
 }
